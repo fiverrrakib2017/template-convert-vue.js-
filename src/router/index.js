d9fb5534cc4e3f9d586d1f0b784ff18e7724ef70
Rakib Mahmud <fiverrrakib2017@gmail.com>
2024-07-03 17:33:05 +0600

    template included completed

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/views/Pages/Home.vue'
+import NotFound from '@/views/Pages/404.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
+    },
+    {
+      path: '/:pathMatch(.*)*', 
+      name: 'NotFound',
+      component: NotFound
     },
   ]
 })
---
+++ src/views/Pages/404.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  // You can add additional logic here if needed
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: red;
+  }
+  </style>
+  
