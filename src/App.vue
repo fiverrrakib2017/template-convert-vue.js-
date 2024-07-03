672c4241b2a26c1fb55f148e45486773a5f6e0fe
Rakib Mahmud <fiverrrakib2017@gmail.com>
2024-07-03 16:48:24 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Master from './views/Master.vue'
+</script>
+
+<template>
+   <Master></Master>
+</template>
+
+<style scoped>
+
+</style>
